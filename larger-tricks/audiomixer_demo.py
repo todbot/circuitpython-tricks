b0c69be2c018573a974870dc335a0fd1b76e872e
Tod Kurt <todbot@users.noreply.github.com>
2022-02-09 13:26:56 -0800

    audiomixer_demo: added fade up & down

--- larger-tricks/audiomixer_demo.py
+++ larger-tricks/audiomixer_demo.py
@@ -7,11 +7,11 @@ import board
 import audiocore
 import audiomixer
 #from audiopwmio import PWMAudioOut as AudioOut  # for RP2040 etc
-from audioio import AudioOut as AudioOut  # for SAMD51 etc
+from audioio import AudioOut as AudioOut  # for SAMD51/M4 etc
 
 num_voices = 2
 
-# audio pin is almost any pin on RP2040, let's do RX (pin 1) (RP2040 GPIO1)
+# audio pin is almost any pin on RP2040, let's do A0 (RP2040 GPIO226) or RX (RP2040 GPIO1)
 # audio pin is A0 on SAMD51 (Trelllis M4, Itsy M4, etc)
 audio = AudioOut(board.A0)
 mixer = audiomixer.Mixer(voice_count=num_voices, sample_rate=22050, channel_count=1,
@@ -32,7 +32,12 @@ mixer.voice[1].play( wave1, loop=True )
 
 time.sleep(1.0)  # let drums play a bit
 
+# fade each channel up and down
+up_down_inc = 0.01
 while True:
-    mixer.voice[1].level = min(max(mixer.voice[1].level + 0.01, 0), 1)
-    mixer.voice[0].level = min(max(mixer.voice[0].level - 0.01, 0), 1)
+    mixer.voice[1].level = min(max(mixer.voice[1].level + up_down_inc, 0), 1)
+    mixer.voice[0].level = min(max(mixer.voice[0].level - up_down_inc, 0), 1)
+    if mixer.voice[0].level == 0 or mixer.voice[1].level == 0:
+        up_down_inc = -up_down_inc
     time.sleep(0.1)
+
