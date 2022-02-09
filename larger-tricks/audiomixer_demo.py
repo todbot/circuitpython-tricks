82adc210070c555a4fe7975ac646cae810bc6175
Tod Kurt <todbot@users.noreply.github.com>
2022-02-09 11:22:53 -0800

    fix audiomixer demo for SAMD51

--- larger-tricks/audiomixer_demo.py
+++ larger-tricks/audiomixer_demo.py
@@ -1,17 +1,19 @@
 # audiomixer_demo.py -- show how to fade up and down playing loops
-# note this causes glitches and crashes on at least RP2040
+# note this causes glitches and crashes on RP2040
 # 9 Feb 2022 - @todbot / Tod Kurt
 
 import time
 import board
 import audiocore
 import audiomixer
-from audiopwmio import PWMAudioOut as AudioOut
+#from audiopwmio import PWMAudioOut as AudioOut  # for RP2040 etc
+from audioio import AudioOut as AudioOut  # for SAMD51 etc
 
 num_voices = 2
 
-# audio pin is RX (pin 1) (RP2040 GPIO1)
-audio = AudioOut(board.RX)
+# audio pin is almost any pin on RP2040, let's do RX (pin 1) (RP2040 GPIO1)
+# audio pin is A0 on SAMD51 (Trelllis M4, Itsy M4, etc)
+audio = AudioOut(board.A0)
 mixer = audiomixer.Mixer(voice_count=num_voices, sample_rate=22050, channel_count=1,
                          bits_per_sample=16, samples_signed=True)
 # attach mixer to audio playback
@@ -34,4 +36,3 @@ while True:
     mixer.voice[1].level = min(max(mixer.voice[1].level + 0.01, 0), 1)
     mixer.voice[0].level = min(max(mixer.voice[0].level - 0.01, 0), 1)
     time.sleep(0.1)
-
