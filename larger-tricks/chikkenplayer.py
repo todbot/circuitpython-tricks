8d8f265f245ed50d5176e1e82a448d4966e91771
Tod Kurt <todbot@users.noreply.github.com>
2022-03-23 15:46:19 -0700

    added knob pickup mode to chikkenplayer

--- larger-tricks/chikkenplayer.py
+++ larger-tricks/chikkenplayer.py
@@ -1,5 +1,6 @@
 # chikkenplayer.py - StreetChicken Remixer by todbot
 # 21 Mar 2021 - @todbot / Tod Kurt, from @jedgarpark idea
+
 #
 import time
 import board
@@ -12,16 +13,17 @@ from audiopwmio import PWMAudioOut as AudioOut
 time.sleep(5) # wait a bit to reduce noise from CIRCUITPY access
 
 wav_files = (
-    "wav/chikken1_161_22k16b.wav", # slowed down & hacked StreetChicken
-    "wav/chikken2_161_22k16b.wav", # fat distorty bassline
-    "wav/chikken3_161_22k16b.wav", # reversed & slowed & warped StreetChicken
-    "wav/chikken4_161_22k16b.wav", # 808 drum pattern (kick + snare only)
-    "wav/chikken5_161_22k16b.wav", # chunky guitar power chords
-    "wav/chikken6_161_22k16b.wav", # ethereal space arpeggios
+  "wav/chikken1_161_22k16b.wav", # slowed down & hacked StreetChicken
+  "wav/chikken2_161_22k16b.wav", # fat distorty bassline
+  "wav/chikken3_161_22k16b.wav", # reversed & slowed & warped StreetChicken
+  "wav/chikken4_161_22k16b.wav", # 808 drum pattern (kick + snare only)
+  "wav/chikken5_161_22k16b.wav", # chunky guitar power chords
+  "wav/chikken6_161_22k16b.wav", # ethereal space arpeggios
 )
 
 # knob to adjust loop levels
 knob = analogio.AnalogIn(board.A3)
+
 # what keys to use 
 keys = keypad.Keys((board.RX, board.SCK, board.MISO, board.MOSI, board.SCL, board.SDA),
                    value_when_pressed=False, pull=True)
@@ -37,14 +39,25 @@ for i in range(len(wav_files)):
   mixer.voice[i].play(wave, loop=True)
   mixer.voice[i].level = 0.0
 
-print("chikenplayer ready")
+print("chikkenplayer ready")
+
 voice_num = None  # which loop level to change if any
+in_pickup = False  # in knob pickup mode or not
+
 while True:
   event = keys.events.get()
   if event:
-    if event.pressed:  voice_num = event.key_number
-    if event.released: voice_num = None
-  if voice_num is not None:  # while voice key is pressed, adjust level
-    val = knob.value
-    mixer.voice[voice_num].level = val / 65535 # convert to 0-1.0
+    if event.pressed:
+      voice_num = event.key_number
+    if event.released:
+      voice_num = None
+      in_pickup = False
 
+  if voice_num is not None:
+    new_val = knob.value
+    if in_pickup:  # if in knob pickup mode, knob controls level
+      mixer.voice[voice_num].level = new_val / 65535 # convert to 0-1.0
+    else:
+      old_val = int(mixer.voice[voice_num].level * 65535) # convert 0-1 to 0-65535
+      if abs(new_val - old_val) < 100: # if we get close to old value,
+        in_pickup = True               # flip the pickup switch until key release
