0b3afd56add77a9b2a865b7f1d50a443cada8dbb
Tod Kurt <todbot@users.noreply.github.com>
2022-07-27 14:50:03 -0700

    add robust keyboard example

---
+++ larger-tricks/robust_keyboard.py
@@ -0,0 +1,38 @@
+# robust_keyboard_code.py -- demonstrate how to deal with USB disconnect on startup and while running
+# 27 Jul 2022 - @todbot / Tod Kurt
+
+import supervisor
+import time
+import board
+import digitalio
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+def usb_connected(): return supervisor.runtime.usb_connected
+
+if usb_connected():  # only attempt keyboard creation if USB 
+    keyboard = Keyboard(usb_hid.devices)
+    keyboard_layout = KeyboardLayoutUS(keyboard)
+
+buttons = []
+for pin in (board.BUTTON_UP, board.BUTTON_SELECT, board.BUTTON_DOWN):
+    switch = digitalio.DigitalInOut(pin)
+    switch.pull = digitalio.Pull.DOWN # defaults to input
+    buttons.append(switch)
+
+keys = [Keycode.A, Keycode.B, Keycode.C]
+
+while True:
+    print("usb:", usb_connected(), "buttons:", [b.value for b in buttons] )
+    for i in range(len(buttons)):
+        #print(buttons[i].value)
+        if buttons[i].value:
+            if usb_connected():
+                try:
+                    keyboard.send( keys[i] ) 
+                except OSError:  # also if USB disconnected 
+                    pass
+       
+    time.sleep(0.1)
